3fe718dfb7a4dcda4545e7e35599cad45d281d0d
gifcompatible <okeydopeness@gmail.com>
2015-12-30 16:15:59 -0500

    Add solution.

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,16 @@
+# Good Guess
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+#create a method that accepts on argument, an integer
+#the method should evaluate to true IF it == 42 ELSE false
+
+def good_guess?(integer)
+  if integer == 42
+    return true
+  else
+    return false
+  end
+end
