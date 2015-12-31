e93a26bc12102b1d9f6f3b5c68297c8ffb79427f
gifcompatible <okeydopeness@gmail.com>
2015-12-30 20:48:40 -0500

    Add solution to concatenate exercise

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,17 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+=begin
+define a method that takes two arrays as parameters
+RETURN a single array by combining the two arrays
+=end
+
+def array_concat(array_1, array_2)
+  return array_1.concat(array_2)
+end
+
+array_concat([1,4,"blue"], ["pinkie", 67])
